99402bb5599d2ffb45366dd6ab548f4e94f52682
Vladimir Andreev <vladimir.andreev@lge.com>
2017-02-07 12:14:30 +0300

    Test 1: code style. Test 2: hash finction added, simple test added

--- strsearch.c
+++ strsearch.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <inttypes.h>
 
 static const long MAX_FILE_SIZE = 128L * 1024L * 1024L;
 static const long INVALID_FILE_SIZE = -1L;
@@ -11,7 +12,7 @@ static const char MIN_CHAR = 32;
 static const char MAX_CHAR = 127;
 static const char CHAR_NEW_LINE = '\n';
 
-bool is_valid_str( char *str) {
+bool is_valid_str (char *str) {
   char *ptr;
 
   for (ptr = str; *ptr; ++ptr) {
@@ -53,33 +54,39 @@ avl_node_t *avl_create_node();
 void avl_insert (avl_tree_t *tree, uint32_t hash);
 
 
-int main( int argc, char** argv) {
+int main (int argc, char** argv) {
     FILE *file = NULL;
     long fsize = 0L;
     char *str = NULL;
     size_t str_len = 0;
     ssize_t str_read = 0;
+    unsigned long line_num = 0L;
+    uint32_t hash = 0;
 
     if (argc < 2) {
       printf("Usage: strsearch filename\n");
       return 0;
     }
+
     file = fopen( argv[1], "r");
     if (!file) {
       printf("Error while opening file: %s\n", argv[1]);
       return 1;
     }
+
     if (fseek( file, 0, SEEK_END) != 0) {
       printf("Error while seek end file: %s\n", argv[1]);
       fclose( file);
       return 1;
     }
+
     fsize = ftell( file);
     if (INVALID_FILE_SIZE == fsize) {
       printf("Error file size: %s\n", argv[1]);
       fclose( file);
       return 1;
     }
+
     if (fsize > MAX_FILE_SIZE) {
       printf("The %s file size %ld bigger than %ld\n", argv[1], fsize, MAX_FILE_SIZE);
       fclose( file);
@@ -88,7 +95,15 @@ int main( int argc, char** argv) {
     rewind( file);
 
     while ((str_read = getline( &str, &str_len, file)) != -1) {
-      printf("Is valid: %d\n", is_valid_str(str));
+      if (is_valid_str( str)) {
+        hash = jenkins_one_at_a_time_hash( str, str_len);
+        printf("Line: %lu hash: %" PRIu32 "\n", line_num, hash);
+        ++line_num;
+      } else {
+        printf("Line: %lu the string contains invalid data: %s", line_num, str);
+        fclose( file);
+        return 0;
+      }
     }
 
     fclose( file);
