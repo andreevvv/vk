c4f942a476f1d096a2804c995bf0c27793a648ee
Vladimir Andreev <vladimir.andreev@lge.com>
2017-02-07 09:24:34 +0300

    bsearch added

---
+++ bsearch.c
@@ -0,0 +1,63 @@
+#include <limits.h>
+#include <stdio.h>
+
+struct vector_int {
+  int    *base;
+  size_t nitems;
+};
+typedef struct vector_int vector_int;
+
+static const int INVALID_INDEX = -1;
+
+int bsearch (vector_int *value, int x, ...) {
+  if (!value) {
+    return INVALID_INDEX;
+  }
+
+  size_t left = 0;
+  size_t middle = 0;
+  size_t right = value->nitems;
+  if (0 >= value->nitems) {
+    return INVALID_INDEX;
+  }
+
+  while (left < right) {
+    middle = (left + right) >> 1;
+    if (value->base[middle] < x) {
+      left = middle + 1;
+    }
+    else if (value->base[middle] > x) {
+      right = middle;
+    }
+    else {
+      while (middle <= right) {
+        if (value->base[middle] > x) {
+          return middle;
+        }
+        ++middle;
+      }
+      return INVALID_INDEX;
+    }
+  }
+  return INVALID_INDEX;
+}
+
+int unittest(int *values, int nitems, int x) {
+    vector_int v;
+    v.base = values;
+    v.nitems = nitems;
+    return bsearch( &v, x);
+}
+
+int main( int argc, char** argv) {
+    int values[] = { INT_MIN, -1000, -100, -1, -1, 0, 1, 2, 2, 10, 100, 100, 1000, INT_MAX };
+    size_t ntitems = sizeof(values) / sizeof(values[0]);
+    printf("result: %d\n", unittest( &values[0], ntitems, INT_MIN));
+    printf("result: %d\n", unittest( &values[0], ntitems, -1000));
+    printf("result: %d\n", unittest( &values[0], ntitems, -2));
+    printf("result: %d\n", unittest( &values[0], ntitems, 0));
+    printf("result: %d\n", unittest( &values[0], ntitems, 5));
+    printf("result: %d\n", unittest( &values[0], ntitems, 100));
+    printf("result: %d\n", unittest( &values[0], ntitems, 1000));
+    printf("result: %d\n", unittest( &values[0], ntitems, INT_MAX));
+}
